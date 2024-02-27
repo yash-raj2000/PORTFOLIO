44b497ee344b1710151c03b5cedf890451d683f6
Yash Raj <yashrajsingh282@gmail.com>
2024-02-27 20:22:13 +0530

    made changes in database

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.post("/addMessage", async (req, res) => {
   try {
     const { fName, lName, email, phone, subject, message } = req.body;
     const result = await pool.query(
-      "INSERT INTO portfoliodata (fName, lName, email, phone, subject, message) VALUES ($1, $2, $3, $4, $5, $6)  RETURNING *",
+      "INSERT INTO formdata (fName, lName, email, phone, subject, message) VALUES ($1, $2, $3, $4, $5, $6)  RETURNING *",
       [fName, lName, email, phone, subject, message]
     );
     res.json(result.rows[0]);
